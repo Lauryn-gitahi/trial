4e17d69b6db6e8c9df4833d3adaa7278de64e14c
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2021-03-01 16:12:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// // let link=document.querySelector('.li');
+
+// // link.addEventListener('click', ShowOrHide);
+
+// // function ShowOrHide(e){
+// // let item=e.target
+// // console.log(item);
+
+// // if(item.classList[0]=="li"){
+// //     let newDiv =document.createElement('div');
+// //     newDiv.classList.add('comp');
+// //     // newDiv.style.display="block"
+// //     newDiv.innerHTML="<p>Hi there!</p>"
+// //     link.appendChild(newDiv);
+// // }
+// // }
+
+// let links=document.getElementById("navbar-links");
+
+// links.addEventListener('click', showAndhide);
+
+// function showAndhide(e){
+// let item=e.target;
+// console.log(item);
+//     // let description= document.createElement("div");
+//     // description.className='text-forensics';
+
+//     // let comp=document.createElement("div");
+//     // comp.className='comp';
+//     // // let p1=document.createElement('p');
+//     // comp.innerHTML='<h2>COMPUTER FORENSICS</h2>'
+//     // comp.innerHTML='<p>The Computer Forensic Analysis process is the methodical approach to examining digital media to establish factual information for civil or criminal matters.</p>'
+
+//         //  links.appendChild(description);
+
+//          if(item.classList[0]==='active'){
+//             let description= document.createElement("div");
+//             description.className='text-forensics';
+//             let comp=document.createElement("div");
+//             comp.className='comp';
+//             comp.innerHTML='<h2>COMPUTER FORENSICS</h2>'
+//             comp.innerHTML='<p>The Computer Forensic Analysis process is the methodical approach to examining digital media to establish factual information for civil or criminal matters.</p>'
+//             links.appendChild(description);
+//         }
+// }
